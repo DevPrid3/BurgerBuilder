54e46291b6f6560790e3dc7040b3b0d18d9e3e3d
DevPrid3 <prid3.costa@gmail.com>
2020-05-26 23:19:33 +0200

    Section 8 Chapter 149

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Aux from "../../hoc/Auxx";
 import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const Layout = (props) => {
-  return (
-    <Aux>
-      <div>Toolbar, SideDrawer, Backdrop</div>
-      <main className={classes.Content}>{props.children}</main>
-    </Aux>
-  );
-};
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  };
+
+  sideDrawClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
 
 export default Layout;
